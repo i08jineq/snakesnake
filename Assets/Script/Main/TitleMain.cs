0722b474d7a5e69387141cb1ac7f5f093ffac53d
ELEQ <eleq@ELEQs-iMac.local>
2017-10-03 04:22:25 +0900

    Add menu scene

---
+++ Assets/Script/Main/TitleMain.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+namespace SnakeSnake {
+    public class TitleMain : MonoBehaviour {
+
+        #region main
+
+        [SerializeField] private Button startButton;
+        private bool isChangingScene;
+
+        #endregion
+
+        #region definition
+
+        private const string GameScene = "GameScene";
+
+        #endregion
+
+        #region initial
+
+        // Use this for initialization
+        void Start() {
+            SetupStartButtonEvent();
+        }
+
+        #endregion
+
+        #region setupEvent
+
+        private void SetupStartButtonEvent() {
+            startButton.onClick.AddListener(ChangeToGameScene);
+        }
+
+        #endregion
+
+        #region update
+
+        // Update is called once per frame
+        void Update() {
+            UpdateInput();
+        }
+
+        private void UpdateInput() {
+            if (Input.GetKeyDown(KeyCode.Return)) {
+                ChangeToGameScene();
+            }
+        }
+
+        #endregion
+
+        #region event
+
+        private void ChangeToGameScene() {
+            if (isChangingScene) {
+                return;
+            }
+            isChangingScene = true;
+            SceneManager.LoadScene(GameScene);
+        }
+
+        #endregion
+    }
+}
